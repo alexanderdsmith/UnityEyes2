63889d92dc4d6d6e83f521f79609629c6d31cba0
brije <brijeshmuthu270900@gmail.com>
2025-02-12 20:09:59 -0600

    Baseline camera functionality implementation

--- Assets/SynthesEyesServer.cs
+++ Assets/SynthesEyesServer.cs
@@ -5,171 +5,244 @@ using System.Net.Sockets;
 using System.Linq;
 using SimpleJSON;
 using System.Collections.Generic;
+using System.IO;              
+using System.Xml.Serialization;
 
+public class SynthesEyesServer : MonoBehaviour
+{
 
-public class SynthesEyesServer : MonoBehaviour {
+    public GameObject lightDirectionalObj;
+    public GameObject eyeballObj;
+    public GameObject eyeRegionObj;
+    public GameObject eyeRegionSubdivObj;
+    public GameObject eyeWetnessObj;
+    public GameObject eyeWetnessSubdivObj;
+    public GameObject eyeLashesObj;
 
-	public GameObject lightDirectionalObj;
+    private EyeballController eyeball;
+    private EyeRegionController eyeRegion;
+    private SubdivMesh eyeRegionSubdiv;
+    private EyeWetnessController eyeWetness;
+    private SubdivMesh eyeWetnessSubdiv;
+    private DeformEyeLashes[] eyeLashes;
 
-	public GameObject eyeballObj;
-	public GameObject eyeRegionObj;
-	public GameObject eyeRegionSubdivObj;
-	public GameObject eyeWetnessObj;
-	public GameObject eyeWetnessSubdivObj;
-	public GameObject eyeLashesObj;
+    private LightingController lightingController;
 
-	private EyeballController eyeball;
-	private EyeRegionController eyeRegion;
-	private SubdivMesh eyeRegionSubdiv;
-	private EyeWetnessController eyeWetness;
-	private SubdivMesh eyeWetnessSubdiv;
-	private DeformEyeLashes[] eyeLashes;
+    public float defaultCameraPitch = 0;
+    public float defaultCameraYaw = 0;
+    public float cameraPitchNoise = Mathf.Deg2Rad * 20;
+    public float cameraYawNoise = Mathf.Deg2Rad * 40;
+    public float defaultEyePitch = 0;
+    public float defaultEyeYaw = 0;
+    public float eyePitchNoise = 30;
+    public float eyeYawNoise = 30;
 
-	private LightingController lightingController;
+    public bool isSavingData = false;
 
-	// Render settings for randomization
-	public float defaultCameraPitch = 0;
-	public float defaultCameraYaw = 0;
-	public float cameraPitchNoise = Mathf.Deg2Rad * 20;
-	public float cameraYawNoise = Mathf.Deg2Rad * 40;
-	public float defaultEyePitch = 0;
-	public float defaultEyeYaw = 0;
-	public float eyePitchNoise = 30;
-	public float eyeYawNoise = 30;
-	
-	// should you save the data or not
-	public bool isSavingData = false;
+    int framesSaved = 0;
 
-	// frame index for saving
-	int framesSaved = 0;
+    public string xmlCameraFilePath = "..\\camera.xml";
 
-	void Start () {
+    void Start()
+    {
 
-		// Initialise SynthesEyes Objects
-		eyeRegion = eyeRegionObj.GetComponent<EyeRegionController> ();
-		eyeball = eyeballObj.GetComponent<EyeballController> ();
-		eyeRegionSubdiv = eyeRegionSubdivObj.GetComponent<SubdivMesh> ();
-		eyeWetness = eyeWetnessObj.GetComponent<EyeWetnessController> ();
-		eyeWetnessSubdiv = eyeWetnessSubdivObj.GetComponent<SubdivMesh> ();
-		eyeLashes = eyeLashesObj.GetComponentsInChildren<DeformEyeLashes> (true);
+        Debug.Log(File.Exists(xmlCameraFilePath));
 
-		lightingController = GameObject.Find ("lighting_controller").GetComponent<LightingController> ();
-	}
+        eyeRegion = eyeRegionObj.GetComponent<EyeRegionController>();
+        eyeball = eyeballObj.GetComponent<EyeballController>();
+        eyeRegionSubdiv = eyeRegionSubdivObj.GetComponent<SubdivMesh>();
+        eyeWetness = eyeWetnessObj.GetComponent<EyeWetnessController>();
+        eyeWetnessSubdiv = eyeWetnessSubdivObj.GetComponent<SubdivMesh>();
+        eyeLashes = eyeLashesObj.GetComponentsInChildren<DeformEyeLashes>(true);
 
-	void RandomizeScene(){
-		
-		eyeball.SetEyeRotation(Random.Range(-eyeYawNoise, eyeYawNoise)+defaultEyeYaw,
-		                       Random.Range(-eyePitchNoise, eyePitchNoise)+defaultEyePitch);
+        lightingController = GameObject.Find("lighting_controller").GetComponent<LightingController>();
 
-        Camera.main.transform.position = SyntheseyesUtils.RandomVec(
-            defaultCameraPitch - cameraPitchNoise, defaultCameraPitch + cameraPitchNoise,
-			defaultCameraYaw - cameraYawNoise, defaultCameraYaw + cameraYawNoise) *10f;
-        Camera.main.transform.LookAt(Vector3.zero);
+        if (!string.IsNullOrEmpty(xmlCameraFilePath) && File.Exists(xmlCameraFilePath))
+        {
+            LoadCameraFromFile(xmlCameraFilePath);
+        }
+    }
 
-	}
+    void RandomizeScene()
+    {
+        eyeball.SetEyeRotation(Random.Range(-eyeYawNoise, eyeYawNoise) + defaultEyeYaw,
+                                Random.Range(-eyePitchNoise, eyePitchNoise) + defaultEyePitch);
 
-    void Update () {
+        if (string.IsNullOrEmpty(xmlCameraFilePath) || !File.Exists(xmlCameraFilePath))
+        {
+            Camera.main.transform.position = SyntheseyesUtils.RandomVec(
+                defaultCameraPitch - cameraPitchNoise, defaultCameraPitch + cameraPitchNoise,
+                defaultCameraYaw - cameraYawNoise, defaultCameraYaw + cameraYawNoise) * 10f;
+            Camera.main.transform.LookAt(Vector3.zero);
+        }
+    }
 
-        if (isSavingData || Input.GetKey("c")) {
+    void Update()
+    {
+        if (isSavingData || Input.GetKey("c"))
+        {
             RandomizeScene();
         }
 
-        if (isSavingData || Input.GetKey("r")) {
+        if (isSavingData || Input.GetKey("r"))
+        {
             eyeRegion.RandomizeAppearance();
             eyeball.RandomizeEyeball();
         }
 
-        if (isSavingData || Input.GetKey("l")) {
+        if (isSavingData || Input.GetKey("l"))
+        {
             lightingController.RandomizeLighting();
         }
 
         eyeRegion.UpdateEyeRegion();
-		eyeRegionSubdiv.Subdivide();
-		eyeWetness.UpdateEyeWetness();
+        eyeRegionSubdiv.Subdivide();
+        eyeWetness.UpdateEyeWetness();
         eyeWetnessSubdiv.Subdivide();
-        foreach (DeformEyeLashes eyeLash in eyeLashes) 
-			eyeLash.UpdateLashes();
+        foreach (DeformEyeLashes eyeLash in eyeLashes)
+            eyeLash.UpdateLashes();
 
-		if (isSavingData || Input.GetKey("s")) {
-        	StartCoroutine(saveFrame ());
-		}
+        if (isSavingData || Input.GetKey("s"))
+        {
+            StartCoroutine(saveFrame());
+        }
 
         if (Input.GetKeyUp("h"))
             GameObject.Find("GUI Canvas").GetComponent<Canvas>().enabled = !GameObject.Find("GUI Canvas").GetComponent<Canvas>().enabled;
-
     }
 
-	private Color parseColor(JSONNode jN){
-		return new Color (jN[0].AsFloat, jN[1].AsFloat, jN[2].AsFloat, 1.0f);
-	}
-	
+    private Color parseColor(JSONNode jN)
+    {
+        return new Color(jN[0].AsFloat, jN[1].AsFloat, jN[2].AsFloat, 1.0f);
+    }
 
-	private Vector3 parseVec(JSONNode jN){
-		return new Vector3 (jN[0].AsFloat, jN[1].AsFloat, jN[2].AsFloat);
-	}
+    private Vector3 parseVec(JSONNode jN)
+    {
+        return new Vector3(jN[0].AsFloat, jN[1].AsFloat, jN[2].AsFloat);
+    }
 
-	
-	private IEnumerator saveFrame(){
-
+    private IEnumerator saveFrame()
+    {
         framesSaved++;
-
-        // We should only read the screen buffer after rendering is complete
         yield return new WaitForEndOfFrame();
-		
-		// Create a texture the size of the screen, RGB24 format
-		int width = Screen.width;
-		int height = Screen.height;
-		Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
-		
-		// Read screen contents into the texture
-		tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
-		tex.Apply();
+
+        int width = Screen.width;
+        int height = Screen.height;
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.Apply();
 
         byte[] imgBytes = tex.EncodeToJPG();
-        System.IO.File.WriteAllBytes(string.Format("imgs/{0}.jpg", framesSaved), imgBytes);
-        //byte[] imgBytes = tex.EncodeToPNG();
-        //System.IO.File.WriteAllBytes(string.Format("imgs/{0}.png", framesSaved), imgBytes);
+        File.WriteAllBytes(string.Format("imgs/{0}.jpg", framesSaved), imgBytes);
 
-        saveDetails (framesSaved);
+        saveDetails(framesSaved);
+        Object.Destroy(tex);
+    }
 
-		Object.Destroy(tex);
-	}
+    private void saveDetails(int frame)
+    {
+        Mesh meshEyeRegion = eyeRegion.transform.GetComponent<MeshFilter>().mesh;
+        Mesh meshEyeBall = eyeball.transform.GetComponent<MeshFilter>().mesh;
 
-	private void saveDetails(int frame){
+        JSONNode rootNode = new JSONClass();
 
-		Mesh meshEyeRegion = eyeRegion.transform.GetComponent<MeshFilter>().mesh;
-		Mesh meshEyeBall = eyeball.transform.GetComponent<MeshFilter>().mesh;
+        JSONArray listInteriorMargin2D = new JSONArray();
+        rootNode.Add("interior_margin_2d", listInteriorMargin2D);
+        foreach (var idx in EyeRegionTopology.interior_margin_idxs)
+        {
+            Vector3 v_3d = eyeRegion.transform.localToWorldMatrix * meshEyeRegion.vertices[idx];
+            listInteriorMargin2D.Add(new JSONData(Camera.main.WorldToScreenPoint(v_3d).ToString("F4")));
+        }
 
-		JSONNode rootNode = new JSONClass();
+        JSONArray listCaruncle2D = new JSONArray();
+        rootNode.Add("caruncle_2d", listCaruncle2D);
+        foreach (var idx in EyeRegionTopology.caruncle_idxs)
+        {
+            Vector3 v_3d = eyeRegion.transform.localToWorldMatrix * meshEyeRegion.vertices[idx];
+            listCaruncle2D.Add(new JSONData(Camera.main.WorldToScreenPoint(v_3d).ToString("F4")));
+        }
 
-		JSONArray listInteriorMargin2D = new JSONArray();
-		rootNode.Add ("interior_margin_2d", listInteriorMargin2D);
-		foreach (var idx in EyeRegionTopology.interior_margin_idxs) {
-			Vector3 v_3d = eyeRegion.transform.localToWorldMatrix * meshEyeRegion.vertices[idx];
-			listInteriorMargin2D.Add(new JSONData(Camera.main.WorldToScreenPoint(v_3d).ToString ("F4")));
-		}
+        JSONArray listIris2D = new JSONArray();
+        rootNode.Add("iris_2d", listIris2D);
+        foreach (var idx in EyeRegionTopology.iris_idxs)
+        {
+            Vector3 v_3d = eyeball.transform.localToWorldMatrix * meshEyeBall.vertices[idx];
+            listIris2D.Add(new JSONData(Camera.main.WorldToScreenPoint(v_3d).ToString("F4")));
+        }
 
-		JSONArray listCaruncle2D = new JSONArray();
-		rootNode.Add ("caruncle_2d", listCaruncle2D);
-		foreach (var idx in EyeRegionTopology.caruncle_idxs) {
-			Vector3 v_3d = eyeRegion.transform.localToWorldMatrix * meshEyeRegion.vertices[idx];
-			listCaruncle2D.Add(new JSONData(Camera.main.WorldToScreenPoint(v_3d).ToString ("F4")));
-		}
-
-		JSONArray listIris2D = new JSONArray();
-		rootNode.Add ("iris_2d", listIris2D);
-		foreach (var idx in EyeRegionTopology.iris_idxs) {
-			Vector3 v_3d = eyeball.transform.localToWorldMatrix * meshEyeBall.vertices[idx];
-			listIris2D.Add(new JSONData(Camera.main.WorldToScreenPoint(v_3d).ToString ("F4")));
-		}
-
-		rootNode.Add("eye_details", eyeball.GetEyeballDetails());
+        rootNode.Add("eye_details", eyeball.GetEyeballDetails());
         rootNode.Add("lighting_details", lightingController.GetLightingDetails());
         rootNode.Add("eye_region_details", eyeRegion.GetEyeRegionDetails());
+        rootNode.Add("head_pose", (Camera.main.transform.rotation.eulerAngles.ToString("F4")));
 
-        rootNode.Add ("head_pose", (Camera.main.transform.rotation.eulerAngles.ToString ("F4")));
-		
-		System.IO.File.WriteAllText (string.Format("imgs/{0}.json", frame), rootNode.ToJSON(0));
+        File.WriteAllText(string.Format("imgs/{0}.json", frame), rootNode.ToJSON(0));
+    }
 
-	}
+    private void LoadCameraFromFile(string file)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(XMLCamera));
+        FileStream stream = new FileStream(file, FileMode.Open);
+        XMLCamera xmlCam = serializer.Deserialize(stream) as XMLCamera;
+        stream.Close();
+
+        Debug.Log("Loading camera settings from XML file: " + file);
+
+        if (xmlCam.Resolution.x > Screen.width || xmlCam.Resolution.y > Screen.height)
+        {
+            Debug.Log($"Resolution exceeds screen dimensions. Width: {xmlCam.Resolution.x}, Height: {xmlCam.Resolution.y}");
+        }
+        else
+        {
+            Screen.SetResolution((int)xmlCam.Resolution.x, (int)xmlCam.Resolution.y, FullScreenMode.FullScreenWindow);
+            Debug.Log($"Resolution set to Width: {xmlCam.Resolution.x}, Height: {xmlCam.Resolution.y}");
+        }
+
+        Camera.main.nearClipPlane = xmlCam.Near;
+        Camera.main.farClipPlane = xmlCam.Far;
+        Camera.main.orthographicSize = xmlCam.OrthographicSize;
+        Camera.main.orthographic = xmlCam.IsOrthographic;
+
+        Debug.Log($"Near Clip Plane: {xmlCam.Near}");
+        Debug.Log($"Far Clip Plane: {xmlCam.Far}");
+        Debug.Log($"Orthographic Size: {xmlCam.OrthographicSize}");
+        Debug.Log($"Is Orthographic: {xmlCam.IsOrthographic}");
+
+        if (!Camera.main.orthographic)
+        {
+            Camera.main.fieldOfView = xmlCam.FieldOfView;
+            Camera.main.usePhysicalProperties = xmlCam.IsPhysicalCamera;
+
+            Debug.Log($"Field of View: {xmlCam.FieldOfView}");
+            Debug.Log($"Is Physical Camera: {xmlCam.IsPhysicalCamera}");
+
+            if (Camera.main.usePhysicalProperties)
+            {
+                Camera.main.focalLength = xmlCam.Focal;
+                Camera.main.sensorSize = xmlCam.SensorSize;
+                Camera.main.lensShift = xmlCam.LensShift;
+                Camera.main.gateFit = xmlCam.GateFit;
+
+                Debug.Log($"Focal Length: {xmlCam.Focal}");
+                Debug.Log($"Sensor Size: X={xmlCam.SensorSize.x}, Y={xmlCam.SensorSize.y}");
+                Debug.Log($"Lens Shift: X={xmlCam.LensShift.x}, Y={xmlCam.LensShift.y}");
+                Debug.Log($"Gate Fit Mode: {xmlCam.GateFit}");
+            }
+        }
+
+        if (xmlCam.UseProjectionMatrix)
+        {
+            Camera.main.projectionMatrix = xmlCam.ProjectionMatrix;
+            Debug.Log("Custom Projection Matrix Applied");
+            Debug.Log(xmlCam.ProjectionMatrix.ToString());
+        }
+
+        Camera.main.transform.position = xmlCam.Position;
+        Camera.main.transform.rotation = Quaternion.Euler(xmlCam.Pitch, xmlCam.Yaw, xmlCam.Roll);
+
+        Debug.Log($"Position: X={xmlCam.Position.x}, Y={xmlCam.Position.y}, Z={xmlCam.Position.z}");
+        Debug.Log($"Rotation (Pitch, Yaw, Roll): Pitch={xmlCam.Pitch}, Yaw={xmlCam.Yaw}, Roll={xmlCam.Roll}");
+
+        Debug.Log("Finished applying camera settings from XML.");
+    }
+
 }
